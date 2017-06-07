25149b7793fdc27f730c54de1cecebe6ab017ee6
Edison <edison@cladian.com>
2017-06-06 21:31:10 -0500

    init

---
+++ class/plate.php
@@ -0,0 +1,27 @@
+<?php
+
+class Plate{
+  // Declare properties
+  public $stringSection='';
+  public $numberSection='';
+
+
+  // Setters
+  public function setString($string){
+    //TODO get string and asign to each class' variable
+  }
+
+  public function setCurrentDate(){
+
+  }
+
+  public function setCurrentTime(){
+
+  }
+
+  public function canBeOnTheRoad(){
+    //TODO
+
+    return TRUE;
+  }
+}
